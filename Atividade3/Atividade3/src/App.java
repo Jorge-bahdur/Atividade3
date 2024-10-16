aafed6370e38aad7d2cd1f94433ce3f9bde809c2
Jorge-bahdur <jorgebahdurjr.73@gmail.com>
2024-10-16 20:47:37 -0300

    Add files via upload

---
+++ Atividade3/Atividade3/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+String expression = "{[()]}";
+if (isBalanced(expression)) {
+System.out.println("A expressão está balanceada.");
+} else {
+System.out.println("A expressão não está balanceada.");
+}
+}
+
+
+
+   
+}
+
+
